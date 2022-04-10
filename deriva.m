556566ebbc3a38db6ce3a03492def891f5b48fa7
Carlo Zambaldo <carlo.zambaldo@gmail.com>
2022-04-10 11:30:54 +0200

    Update deriva.m

--- deriva.m
+++ deriva.m
@@ -1,12 +1,20 @@
 function [derivata] = deriva(f,ordine)
-% [derivata] = DERIVA(f,ordine)
-% f: funzione in una variabile
-% ordine: ordine della derivata finale (derivata prima, seconda, ...)
-% la derivata viene effettuata secondo la variabile di f
+    % [derivata] = DERIVA(f,ordine)
+    % f: funzione in una variabile
+    % ordine: ordine della derivata finale (derivata prima, seconda, ...)
+    % la derivata viene effettuata secondo la variabile di f
+    %
+    % attenzione:
+    %   d[@(x)x]/dx = @()1   ->   ovvero non ha più argomento
+
+
+    % Software by Carlo Zambaldo (info@carlozambaldo.it)
+    % This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+    
 
     syms x;
     f = f(x);
 
     derivata = diff(f,x,ordine);
-    derivata = matlabFunction(derivata);
+    derivata = matlabFunction(derivata); 
 end
