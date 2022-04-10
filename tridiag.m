bdec1c6959ec91eaf325d7ffe06a0f0184ac3c4a
Carlo Zambaldo <carlo.zambaldo@gmail.com>
2022-04-10 11:33:31 +0200

    Update tridiag.m

--- tridiag.m
+++ tridiag.m
@@ -4,6 +4,10 @@ function [T] = tridiag(a, b, c, n)
         % T = tridiag(a, b, c, n) returns an n by n matrix that has 
         % a, b, c as the subdiagonal, main diagonal, and superdiagonal 
         % entries in the matrix.
+        
+    % Software by Carlo Zambaldo (info@carlozambaldo.it)
+    % This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+    
 
         T = b*diag(ones(n,1)) + c*diag(ones(n-1,1),1) + a*diag(ones(n-1,1),-1);
 end
