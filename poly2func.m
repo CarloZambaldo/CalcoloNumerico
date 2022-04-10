0e75889396e10295056cc07c598b21851e3a5740
Carlo Zambaldo <carlo.zambaldo@gmail.com>
2022-04-10 12:32:34 +0200

    Update poly2func.m

--- poly2func.m
+++ poly2func.m
@@ -1,4 +1,12 @@
-function p_fun = poly2func(p_vect)
+function [p_fun] = poly2func(p_vect)
+ %  [p_fun] = poly2func(p_vect)
+ %
+ % converte un polinomio (definito dal vettore dei coefficienti) in anonymous function 
+ 
+ 
+    % Software by Carlo Zambaldo (info@carlozambaldo.it)
+    % This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+    
     p = symfun(p_vect);
     p_fun = matlabFunction(p);
 end
