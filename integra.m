eb14e2a7c994f68c50ee1c14a3519e6ef572ae77
Carlo Zambaldo <carlo.zambaldo@gmail.com>
2022-04-10 12:19:03 +0200

    Update integra.m

--- integra.m
+++ integra.m
@@ -1,8 +1,16 @@
 function f_integrata = integra(f)
     % [f_integrata] = INTEGRA(f)
-    % f: funzione in una variabile
-    % ordine: ordine della derivata finale (derivata prima, seconda, ...)
+    %
+    % f        =  funzione in una variabile
+    % ordine   =  ordine della derivata finale (derivata prima, seconda, ...)
+    %
     % la derivata viene effettuata secondo la variabile di f
+    %
+    % l'output f_integrata è a sua volta un'anonymous function
+    % attenzione: f_integrata NON presenta la costante "+c".
+
+    % Software by Carlo Zambaldo (info@carlozambaldo.it)
+    % This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
     
     syms x;
     f = f(x);
