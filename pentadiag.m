0548e0afb4f8e95f2988c62af67aa0f850df27ea
Carlo Zambaldo <carlo.zambaldo@gmail.com>
2022-04-10 12:30:25 +0200

    Update pentadiag.m

--- pentadiag.m
+++ pentadiag.m
@@ -6,6 +6,9 @@ function [T] = pentadiag(a, b, c, d, e, n)
         % superdiagonal and supersuperdiagonal
         % entries in the matrix.
 
+    % Software by Carlo Zambaldo (info@carlozambaldo.it)
+    % This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+    
         T = c*diag(ones(n,1)) + d*diag(ones(n-1,1),1) + b*diag(ones(n-1,1),-1) + ...
             a*diag(ones(n-2,1),-2) + e*diag(ones(n-2,1),2);
 end
